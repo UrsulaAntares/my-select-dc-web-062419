089b9b7689eaf0a273300bc7440572d89572a56d
Ursula Miller <ursula.miller@gmail.com>
2019-06-16 21:30:53 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,24 @@
 def my_select(collection)
  # your code here!
 end
+
+
+
+
+
+
+#def my_collect(array)
+#  i = 0
+#  new_array= []
+#  while i < array.length
+#    new_name = yield array[i]
+#    new_array << new_name
+#    i += 1
+#  end
+#  new_array 
+#end  
+  
+  
+#my_collect(array) do |name|
+#  name.split(" ").first 
+#end
